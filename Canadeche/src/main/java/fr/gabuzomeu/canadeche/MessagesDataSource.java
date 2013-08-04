c104bea4e8dd5f817769c9ae43abeeeb8570da6f
yneveu <yann-github@gabuzomeu.fr>
2013-08-05 00:30:05 +0200

    Add notifications on login< and quietBoard config option to notify on new post on quiet boards

--- Canadeche/src/main/java/fr/gabuzomeu/canadeche/MessagesDataSource.java
+++ Canadeche/src/main/java/fr/gabuzomeu/canadeche/MessagesDataSource.java
@@ -1,7 +1,12 @@
 package fr.gabuzomeu.canadeche;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
@@ -33,6 +38,8 @@ public class MessagesDataSource {
             SqliteHelper.COLUMN_POST_ID
              };
 
+    private Context context;
+    SharedPreferences prefs;
 
     public static final String COLUMN_BOARD_ID = "board_id";
     public static final String COLUMN_ID = "id";
@@ -42,9 +49,11 @@ public class MessagesDataSource {
     public static final String COLUMN_LOGIN = "login";
     public static final String COLUMN_POST_ID = "post_id";
 
-    public MessagesDataSource(Context context) {
+    public MessagesDataSource(Context _context) {
+        context = _context;
         dbHelper = new SqliteHelper( context);
-        debug = PreferenceManager.getDefaultSharedPreferences(context).getBoolean( "pref_debug", false);
+        prefs = PreferenceManager.getDefaultSharedPreferences( context);
+        debug = prefs.getBoolean("pref_debug", false);
     }
 
     public void open() throws SQLException {
@@ -78,11 +87,52 @@ public class MessagesDataSource {
             values.put( SqliteHelper.COLUMN_BOARD_ID, inputMessage.getBoard());
             long insertId = database.insert( SqliteHelper.MESSAGES_TABLE, null, values);
             cursor.close();
+
+            String comparedLogin = prefs.getString( "boardconfig_"+ inputMessage.getBoard() + "_edittext_boardlogin", "improbablestring87984qsfqezfeqz" ) + "<";
+            String comparedLoginEncoded = prefs.getString( "boardconfig_"+ inputMessage.getBoard() + "_edittext_boardlogin", "improbablestring87984qsfqezfeqz" ) + "&amp;lt;";
+            if( inputMessage.getMessage().contains( comparedLogin ) || inputMessage.getMessage().contains( comparedLoginEncoded )  ){
+                if( debug)
+                    Log.d( TAG, "Bigorno  " + "boardconfig_"+ inputMessage.getBoard() + "_edittext_boardlogin") ;
+                    notifyOnNewPost( inputMessage, "New personal message on board" + inputMessage.getBoard() );
+            }
+
+
+            if( prefs.getBoolean( "boardconfig_"+ inputMessage.getBoard() + "_checkbox_quietboard", false ) ){
+
+                if( debug)
+                    Log.d( TAG, "Quietboard  " + "boardconfig_"+ inputMessage.getBoard() + "_checkbox_boardlogin") ;
+                notifyOnNewPost( inputMessage, "New message on board" + inputMessage.getBoard() );
+            }
             return insertId;
         }
         cursor.close();
         return -1;
     }
+
+        private void notifyOnNewPost( Message inputMessage, String reason){
+            final NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            Intent intent = new Intent( context, MainActivityDrawer.class);
+            PendingIntent pIntent = PendingIntent.getActivity( context, 0, intent, 0);
+
+            //Format norloge
+            String sClock= String.valueOf( inputMessage.getTime());
+            final String hour=sClock.substring( 8, 10);
+            final String minutes=sClock.substring(10,12);
+            final String seconds=sClock.substring( 12,14);
+            String cNorloge =  hour + ":" + minutes + ":" + seconds;
+
+
+            Notification noti = new Notification.Builder( context)
+                    .setContentTitle( reason)
+                    .setContentText(inputMessage.getBoard() + " (" + cNorloge + ") : " + inputMessage.getMessage())
+                    .setSmallIcon(R.drawable.ic_launcher)
+                    .setContentIntent(pIntent).build();
+
+
+            noti.flags |= Notification.FLAG_AUTO_CANCEL;
+            notificationManager.notify(0, noti);
+        }
 
 /**TODO: limit par tribune*/
     public List<Message> getAllMessages( String inputBoard){
